ec1372f5f8f1374ce658b18b2edbc4ac546789ae
Sonic-2004 <167118103+Sonic-2004@users.noreply.github.com>
2024-04-21 17:25:04 +0530

    Update staff_management.h

--- staff_management.h
+++ staff_management.h
@@ -4,5 +4,6 @@
 // Function prototypes
 void add_staff(char* staff_type, char* name, char* mobile_number, int on_holiday);
 void display_staff();
+void add_staff_case(); // Added function prototype for case 11
 
 #endif /* STAFF_MANAGEMENT_H */
