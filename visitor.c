b74add6fd8293ddc6a8aa0cdbb4af7dd77d06533
Sonic-2004 <167118103+Sonic-2004@users.noreply.github.com>
2024-04-21 12:34:53 +0530

    Update visitor.c

--- visitor.c
+++ visitor.c
@@ -1,4 +1,4 @@
-#include "Visitor.h"
+#include "visitor.h"
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
@@ -9,14 +9,20 @@ int numVisitors = 0;
 void loadVisitors() {
     FILE *file = fopen("visitors.csv", "r");
     if (file != NULL) {
+        printf("Visitor Log:\n");
+        printf("ID\tName\tPurpose\tStatus\tTimestamp\n");
+        printf("---------------------------------------------\n");
+        
         int i = 0;
         while (fscanf(file, "%d,%[^,],%[^,],%[^,],%ld\n", &visitors[i].id, visitors[i].name, visitors[i].purpose, visitors[i].status, &visitors[i].timestamp) != EOF) {
+            printf("%d\t%s\t%s\t%s\t%ld\n", visitors[i].id, visitors[i].name, visitors[i].purpose, visitors[i].status, visitors[i].timestamp);
             i++;
         }
         fclose(file);
         numVisitors = i; // Update the number of visitors
     }
 }
+
 
 void saveVisitors() {
     FILE *file = fopen("visitors.csv", "w");
@@ -57,39 +63,4 @@ void addVisitor() {
     fclose(file);
 
     numVisitors++;
-}
-
-#include "Visitor.h"
-#include <stdio.h>
-
-int main() {
-    int choice;
-    loadVisitors();
-
-    do {
-        printf("\nVisitor Management\n");
-        printf("1. View Visitor Log\n");
-        printf("2. Add New Visitor\n");
-        printf("3. Exit\n");
-        printf("Enter your choice: ");
-        scanf("%d", &choice);
-
-        switch (choice) {
-            case 1:
-                // Code to view visitor log
-                break;
-            case 2:
-                addVisitor();
-                break;
-            case 3:
-                saveVisitors();
-                printf("Exiting...\n");
-                break;
-            default:
-                printf("Invalid choice!\n");
-                break;
-        }
-    } while (choice != 3);
-
-    return 0;
 }
