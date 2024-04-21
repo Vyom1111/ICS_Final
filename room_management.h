dc0b4905b5c5854b560c3b6dba3c3767f8e27f1b
Sonic-2004 <167118103+Sonic-2004@users.noreply.github.com>
2024-04-21 17:26:34 +0530

    Update room_management.h

--- room_management.h
+++ room_management.h
@@ -1,8 +1,8 @@
 #ifndef ROOM_MANAGEMENT_H
 #define ROOM_MANAGEMENT_H
+
 #define MAX_ROOM_NUMBER_LENGTH 10
-// Include necessary header files
-//#include "main.h"
+
 typedef struct {
     char roomNumber[MAX_ROOM_NUMBER_LENGTH];
     int bedCount;
@@ -13,9 +13,7 @@ typedef struct {
     int bulbCount;
 } Room;
 
-// Function declarations
 Room* initializeRoomData(int numRooms);
-// void assignRoom(Room *rooms, int numRooms, char *studentName);
 void displayRoomDetails(Room *rooms, int numRooms);
 
 #endif /* ROOM_MANAGEMENT_H */
