84c6c1a9c94e808d9b4e7e89c5412691831692f1
Sonic-2004 <167118103+Sonic-2004@users.noreply.github.com>
2024-04-21 17:28:46 +0530

    Update inventory_management.h

--- inventory_management.h
+++ inventory_management.h
@@ -2,7 +2,11 @@
 #define INVENTORY_MANAGEMENT_H
 
 // Function prototypes
+void log_inventory_issue(char* student_name, char* item);
 void issue_inventory_item(char* student_name, char* item);
 void display_inventory();
+void prompt_issue_inventory_item();
+void add_inventory_item(char* item, int quantity);
+void add_inventory_item_case();
 
 #endif /* INVENTORY_MANAGEMENT_H */
